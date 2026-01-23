df4076f979b7fcb2050ff218b2239218c7871923
Babajide789 <yahayababajide@gmail.com>
2026-01-23 04:05:46 -0500

    reverse debugging try 1

---
+++ src/app/properties/[id]/notFound.tsx
@@ -0,0 +1,10 @@
+
+
+export default function NotFoundPage() {
+    return(
+        <>
+            <h1>Property Not Found</h1>
+            <p>The property you are looking for does not exist.</p>
+        </>
+    )
+}
